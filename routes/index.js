479a06c8b42d3303631924f2b540537ac19ad4c0
mlcundayag <mlcundayag@gmail.com>
2022-10-09 01:20:54 -1000

    set up main routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+//import main routes
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+//set up main routes
+router.use('/', homeRoutes);
+router.use('/blog', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
