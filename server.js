c26d98bc7d78610583f329df75f18dd0b77f0819
mlcundayag <mlcundayag@gmail.com>
2022-10-08 23:55:51 -1000

    set up handlebars on server

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 //import dependencies
-const { urlencoded } = require('express');
 const express = require('express');
-//import sequelize connection
+const exphbs = require('express-handlebards');
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// connect routes and sequelize
 const sequelize = require('./config/connection');
-//import routes
 const routes = require('./routes');
 
+//create connect helpers
+
+const hbs = exphbs.create({ helpers });
+
+//connection session
+const sess = {
+    secret: 'super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitiliazed: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//initialize server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(session(sess));
 
-//connect to routes
+//use routes
 app.use(routes);
+
+//set handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //sequelize server
 sequelize.sync({ force: false }).then(() => {
