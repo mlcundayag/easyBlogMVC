e4186706a786540d2e7d02e3024d666e52dcddd4
mlcundayag <mlcundayag@gmail.com>
2022-10-09 01:27:28 -1000

    create plan for api routes

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+//main api routes
+const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+
+//set up main routes
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+router.use('/post', postRoutes);
+
+module.exports = router;
