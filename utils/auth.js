2d13e39bed8badfd5f80788109c4b7ebaff877ac
mlcundayag <mlcundayag@gmail.com>
2022-10-10 00:51:43 -1000

    update authorization utils

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,10 @@
 const withAuth = (req, res, next) => {
-    //redirect if user is not logged in
-    if (!req.session.userID) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+  //redirect if user is nog logged in
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
